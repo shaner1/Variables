43e0100f1c95418aa007786831ca9d8d1fb3afc9
Shane Rylands <Shane@Shanes-MacBook-Pro.local>
2020-02-04 23:14:50 +0000

    Program takes in a users sentence and outputs every second letter

---
+++ Variables.py
@@ -0,0 +1,10 @@
+#Task 3: variables
+#Program takes in a users sentence and outputs every second letter 
+
+
+sentence = input("Please enter a sentence: ")
+
+print(sentence)
+
+print(sentence[::-2])
+
